c21ae8be468f0f1dfa86903ec988d97bff036b69
Guillaume Denis <denisgme@gmail.com>
2016-12-02 18:52:04 +0100

    add pam change

--- setup.sh
+++ setup.sh
@@ -63,7 +63,28 @@ function change_umask {
 
 # Change PAM to harden auth through apps
 function change_pam {
+    printf '#%PAM-1.0\n
+    # This file is auto-generated.\n
+    # User changes will be destroyed the next time authconfig is run.\n
+    auth        required      pam_env.so\n
+    auth        sufficient    pam_unix.so nullok try_first_pass\n
+    auth        requisite     pam_succeed_if.so uid >= 500 quiet\n
+    auth        required      pam_deny.so\n
+    auth        required      pam_tally2.so deny=3 onerr=fail unlock_time=60\n
     
+    account     required      pam_unix.so\n
+    account     sufficient    pam_succeed_if.so uid < 500 quiet\n
+    account     required      pam_permit.so\n
+    account     required      pam_tally2.so per_user\n
+    
+    password    requisite     pam_cracklib.so try_first_pass retry=3 minlen=9 lcredit=-2 ucredit=-2 dcredit=-2 ocredit=-2\n
+    password    sufficient    pam_unix.so sha512 shadow nullok try_first_pass use_authtok remember=10\n
+    password    required      pam_deny.so\n
+    
+    session     optional      pam_keyinit.so revoke\n
+    session     required      pam_limits.so\n
+    session     [success=1 default=ignore] pam_succeed_if.so service in crond quiet use_uid\n
+    session     required      pam_unix.si\n' > /etc/pam.d/system-auth
 }
 
 # kick inactive users after 20 minutes
